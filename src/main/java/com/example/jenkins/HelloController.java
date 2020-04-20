c093adbe8d95a03ec643c65cf488fce13ab0a460
赵从亮 <zcl13993@ly.com>
2020-04-20 11:03:44 +0800

    update

--- src/main/java/com/example/jenkins/HelloController.java
+++ src/main/java/com/example/jenkins/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("hello")
     public String hello() {
-        return "hello world~3";
+        return "hello world~4k";
     }
 }
